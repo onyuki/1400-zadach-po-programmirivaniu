026065cb36a22e44518317751a5a885eda46e007
yuki <starikovich.marina@gmail.com>
2025-10-24 18:59:18 +0300

    Add files via upload

---
+++ 1.14-1.17.py
@@ -0,0 +1,47 @@
+﻿#Задание 1.14
+
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+c = input("Введите третье число: ")
+
+print(a, b, c, sep="  ")  # два пробела между числами
+
+# 1.15
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+c = input("Введите третье число: ")
+d = input("Введите четвёртое число: ")
+
+print(a, b, c, d)
+
+
+# 1.16
+t = int(input("Введите t: "))
+v = int(input("Введите v: "))
+x = int(input("Введите x: "))
+y = int(input("Введите y: "))
+
+print(f"{5} {t}")
+print(f"{7} см\n")
+
+print(f"{100} {t}")
+print(f"{1949} {v}\n")
+
+print(f"{x} {25}")
+print(f"{x} {y}")
+
+
+# 1.17
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+x = int(input("Введите x: "))
+y = int(input("Введите y: "))
+
+print(f"{2} кг")
+print(f"{13} г\n")
+
+print(f"{a} {1}")
+print(f"{19} {b}\n")
+
+print(f"{x} {y}")
+print(f"{5} {y}")
